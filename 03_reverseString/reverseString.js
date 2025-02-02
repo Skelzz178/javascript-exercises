9a951e28f2003dc7db5a30ecd03131ac1c48682a
Kelvin Seow <kelvinseow.web@gmail.com>
2025-02-02 16:43:11 +0800

    Completed exercise 03-reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  stringToArray = string.split("");
+  stringToArray.reverse();
+  let processedString = stringToArray.join("");
 
+  return processedString;
 };
 
 // Do not edit below this line
