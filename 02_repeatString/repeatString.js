3c9ef3fc685ec1b2d949de2db9f05edc52671561
Kelvin Seow <kelvinseow.web@gmail.com>
2025-02-02 16:27:25 +0800

    Completed exercise 02-repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let fullString = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      fullString += string;
+    }
+  } else {
+    return "ERROR";
+  }
 
+  return fullString;
 };
 
 // Do not edit below this line
