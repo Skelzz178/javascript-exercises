980346ecee22bd314afd511385eef65e9ddc89af
Kelvin Seow <kelvinseow.web@gmail.com>
2025-02-02 17:44:32 +0800

    Completed exercise 04-removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...remove) {
+  for (item of remove) {
+    // Loops until item to be removed cannot be found in the array
+    while (array.indexOf(item) !== -1) {
+      array.splice(array.indexOf(item), 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
