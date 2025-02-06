1126f2e83b8cb3e5860baf460dc739c949c89253
Kelvin Seow <kelvinseow.web@gmail.com>
2025-02-07 00:18:58 +0800

    Completed exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Processed string to lower case and remove punctuation
+  newString = str.toLowerCase();
+  newString = newString.replace(/[^\w\s]|_/g, "");
+  // Transform string to array and remove blank items
+  newArray = newString.split("");
+  newArray = newArray.filter((a) => a !== " ");
 
+  // Counter for loop will be half of array length
+  toLoopFor = Math.floor(newArray.length / 2);
+  console.log(newArray);
+  console.log(toLoopFor);
+  // Loop to compare the front and end of the array
+  for (let i = 0; i < toLoopFor; i++) {
+    if (newArray[i] === newArray[newArray.length - i - 1]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
