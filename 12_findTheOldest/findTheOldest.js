6f66117cc3b5e69e3c8d53060088b78ed4f51a7d
Kelvin Seow <kelvinseow.web@gmail.com>
2025-02-08 01:02:54 +0800

    Completed exercise 12-findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // Create new array with new property "age"
+  peopleWithAge = people.map((item) => {
+    // Add age based on todays Year if yearOfDeath not defined
+    if (!item.yearOfDeath) {
+      let currentDate = new Date();
+      let currentYear = currentDate.getFullYear();
+      item.age = currentYear - item.yearOfBirth;
+      return item;
+    } else {
+      item.age = item.yearOfDeath - item.yearOfBirth;
+      return item;
+    }
+  });
 
+  // Set current oldest person to first person in array
+  let oldestPerson = peopleWithAge[0];
+  // Compare between array items to set oldest person
+  for (let i = 0; i < peopleWithAge.length - 1; i++) {
+    if (
+      oldestPerson.age > peopleWithAge[i + 1].age ||
+      oldestPerson.age === peopleWithAge[i + 1].age
+    ) {
+      continue;
+    } else {
+      oldestPerson = peopleWithAge[i + 1];
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
