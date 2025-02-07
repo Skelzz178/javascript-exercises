dd2ce911d699bbcbda5936f5da71351e3b90e790
Kelvin Seow <kelvinseow.web@gmail.com>
2025-02-07 18:37:14 +0800

    Completed exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = parseInt(num);
 
+  if (num === 0) {
+    return 0;
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  let prevTotal = 0;
+  let prevAnswer = 1;
+
+  for (let i = 1; i <= num; i++) {
+    currentAnswer = prevTotal + prevAnswer;
+
+    prevAnswer = prevTotal;
+    prevTotal = currentAnswer;
+  }
+
+  return currentAnswer;
 };
 
 // Do not edit below this line
