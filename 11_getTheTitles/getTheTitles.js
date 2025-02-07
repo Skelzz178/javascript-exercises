c995911f893fa529ff88d2cb9b435dc4b607a169
Kelvin Seow <kelvinseow.web@gmail.com>
2025-02-07 18:54:01 +0800

    Completed exercise 11-getthetitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  const bookTitles = [];
 
+  books.forEach((item) => bookTitles.push(item.title));
+
+  return bookTitles;
 };
 
 // Do not edit below this line
